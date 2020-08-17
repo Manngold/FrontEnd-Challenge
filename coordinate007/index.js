f852d17a62aa667355bf9507b69f426b3f7bc12a
Manngold <jsw4820@gmail.com>
2020-08-17 12:55:44 +0900

    Optimization using translate

--- coordinate007/index.js
+++ coordinate007/index.js
@@ -3,16 +3,19 @@ const span = document.querySelector('.coordinate');
 const horizontal = document.querySelector('.horizontal');
 const vertical = document.querySelector('.vertical');
 const target = document.querySelector('.target');
+const targetRect = target.getBoundingClientRect();
+const targetWidth = targetRect.width / 2;
+const targetHeight = targetRect.height / 2;
 
 body.addEventListener('mousemove', (event) => {
   x = event.clientX;
   y = event.clientY;
 
+  horizontal.style.transform = `translateY(${y}px)`;
+  vertical.style.transform = `translateX(${x}px)`;
+  target.style.transform = `translate(${x - targetWidth}px ,${
+    y - targetHeight
+  }px)`;
+  span.style.transform = `translate(${x}px, ${y}px)`;
   span.innerHTML = `${x}px , ${y}px`;
-  span.style.top = `${y}px`;
-  span.style.left = `${x}px`;
-  horizontal.style.top = `${y}px`;
-  vertical.style.left = `${x}px`;
-  target.style.top = `${y}px`;
-  target.style.left = `${x}px`;
 });
